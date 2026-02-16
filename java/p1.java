33d33feee85fbf0408dd115793c2c0ba60370259
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-02-16 19:08:37 +0530

    changed

--- java/p1.java
+++ java/p1.java
@@ -3,5 +3,6 @@ package Preparation.java;
 public class p1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello bro this is my first git psuh");
     }
 }
