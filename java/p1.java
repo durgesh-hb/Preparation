13e40d660e9e8fac3d5a75cb231986ddb6debce0
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-02-16 21:41:40 +0530

    come on

--- java/p1.java
+++ java/p1.java
@@ -4,6 +4,10 @@ public class p1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello bro this is my first git psuh");
+<<<<<<< HEAD
         System.out.println("If you are seeing this means your in the branch 2");
+=======
+        System.out.println("If you are seeing this means your in branch 2");
+>>>>>>> f20b630 (branch 2 is created)
     }
 }
