d1e8c551684ee4101dbf8ecfc1d7a7c0349ac8c4
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-02-18 19:11:29 +0530

    binary search

---
+++ java/binary_search.java
@@ -0,0 +1,29 @@
+package Preparation.java;
+
+public class binary_search{
+    public static void main(String[] args) {
+        int[] arr={4, 6, 1, 2, 16, 7, 8, 12,};
+        int target=12;
+        int ans=biinear_search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int biinear_search(int[] arr, int target){
+         int start = 0;
+         int end = arr.length - 1;
+
+        while(start <= end) {
+
+        int mid = start + (end - start) / 2;
+
+        if (target < arr[mid]) {
+            end = mid - 1;
+        } else if (target > arr[mid]) {
+             start = mid + 1;
+        } else {
+         return mid;
+     }
+     }
+     return -1;
+}
+}
