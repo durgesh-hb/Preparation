0891d342b4afd8e35876d1487209e61ab1b3258f
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-02-25 20:27:30 +0530

    coded the logic of selection sort in code

---
+++ java/sorting/selection_sort.java
@@ -0,0 +1,22 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class selection_sort {
+    public static void main(String[] args) {
+        
+    
+    }
+    static void selection(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            int smallest=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[smallest]){
+                    smallest=j;
+                }
+            }
+            
+        }
+    
+}
+}
