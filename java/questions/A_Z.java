c7c1499fce66981fce2f9afed1fd28eea4277efe
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-07 20:49:27 +0530

    To print A to Z

---
+++ java/questions/A_Z.java
@@ -0,0 +1,11 @@
+package questions;
+
+public class A_Z {
+
+    public static void main(String[] args) {
+        for(char c='a'; c<='z'; c++){
+            System.out.print(c + " ");
+        }
+    }
+    
+}
