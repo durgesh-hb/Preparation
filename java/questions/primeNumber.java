acf166d91ae7dee7be77e2d0e78bbe93aa64a97c
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-07 20:53:24 +0530

    prime numbers

---
+++ java/questions/primeNumber.java
@@ -0,0 +1,22 @@
+package questions;
+import java.util.Scanner;
+public class primeNumber{
+    
+    public static void main(String[] args) {
+        Scanner sc=new  Scanner(System.in);
+		
+		int n=sc.nextInt();
+		if (prime(n)) {
+			System.out.println("is prime");
+		}else {
+			System.out.println("is not prime ");
+		}
+    }
+
+    static boolean prime(int n) {
+        for (int i=2;i<n;i++)
+            if (n%i==0)
+                return false;
+        return true;
+    }
+}
