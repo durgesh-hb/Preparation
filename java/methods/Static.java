f30cf107b08c175782b4fb027be82ada3cba215b
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-10 20:29:46 +0530

    static method

---
+++ java/methods/Static.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class Static {
+
+    static void show() {
+        System.out.println("Static Method");
+    }
+
+    public static void main(String[] args) {
+
+        Static.show(); // calling static method directly
+
+    }
+
+}
