514edb16eaed125adbfb31ac600817bdfcddb171
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-10 20:40:57 +0530

    add to arrays verticaily

---
+++ java/Arrays/add2arrays.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class add2arrays {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a[]= {2,3,4}; 
+		int b[]= {4,5,6};
+		
+		for (int i=0;i<a.length;i++) {
+			
+			System.out.print(a[i]+b[i]); //2+4, 3+5, 4+6
+		}
+	}
+}
