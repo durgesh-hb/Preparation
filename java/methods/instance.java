139e250d827b2e23314b29656f08956ef05ca03a
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-10 20:32:35 +0530

    methods

---
+++ java/methods/instance.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class instance {
+    
+    void show() {
+        System.out.println("Instance Method");
+    }
+
+    public static void main(String[] args) {
+
+        instance obj = new instance(); // create object
+        obj.show();            // calling method
+
+    }
+}
