fbc791050276a90bc454283064014b11c07e84ca
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-13 19:52:23 +0530

    default constructors

---
+++ java/constructors/Default.java
@@ -0,0 +1,14 @@
+package constructors;
+
+public class Default {
+
+    Default(){
+		System.out.println("Hello bro");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Default s=new Default();
+	}
+     
+}
