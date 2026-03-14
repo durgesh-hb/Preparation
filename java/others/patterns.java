fdbc8312bbddf22537bec7197bfa7020941e604a
Durgesh Gowda HB <117710393+DurgeshGowda-HB@users.noreply.github.com>
2026-03-14 20:47:08 +0530

    patterns

--- java/others/patterns.java
+++ java/others/patterns.java
@@ -4,7 +4,7 @@ public class patterns{
     public static void main(String[] args) {
         pattern1(5);
     }
-
+ 
     static void pattern1(int n){
         //outter loop for row
         for(int row=0; row<=n; row++){
