caa47d2744b903e29abdc27b4e131cff4ba72e04
Durgesh Gowda HB <117710393+DurgeshGowda-HB@users.noreply.github.com>
2026-03-14 20:44:07 +0530

    mutable string

--- java/string_classes/string_buffer.java
+++ java/string_classes/string_buffer.java
@@ -2,6 +2,22 @@ package string_classes;
 
 public class string_buffer {
     public static void main(String[] args) {
-        
+
+        StringBuffer s=new StringBuffer();
+		
+		s.append("nithin bharadwaj");
+		System.out.println("String orignal is :"+s);
+		
+		s.insert(0,"KS ");
+		System.out.println("after insert:"+s);
+		
+		s.replace(3, 4, "NI");
+		System.out.println("after the replace :"+s);
+		
+		s.reverse();
+		System.out.println("its a reverse string "+s);
+		
+		s.delete(0, 1);
+		System.out.println("after delete ");
     }
 }
