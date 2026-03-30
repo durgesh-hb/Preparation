3f058f674e5cde18582346f91a5635712bcbb342
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-03-30 20:31:07 +0530

    Linked Hash Set

---
+++ java/collection/linkedhashset.java
@@ -0,0 +1,27 @@
+package collection;
+import java.util.*;
+public class linkedhashset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedHashSet <String> sh=new LinkedHashSet<>();
+		
+		sh.add("ravi");
+		sh.add("kumar");
+		sh.add("sunil");
+		sh.add("raju");
+		
+		System.out.println(sh);
+		
+		sh.remove("kumar");
+		
+		for(String value: sh) {
+			System.out.print(value + ",");
+		}
+		
+		//same as set can't updated and access index 
+		
+	}
+
+}
