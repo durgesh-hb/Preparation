67d0a76e5a30ba6b158670be6f685106ea289ac3
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-04-13 12:44:34 +0530

    using set

--- java/questions/arrays/remove_duplicate.java
+++ java/questions/arrays/remove_duplicate.java
@@ -1,13 +1,11 @@
 package questions.arrays;
-
+import java.util.*;
 public class remove_duplicate {
     public static void main(String[] args) {
         
         int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
-        removedup(arr);
+        usingset(arr);
 
-        
-        
     }
 
     static int remove(int[] arr) {
@@ -22,35 +20,55 @@ public class remove_duplicate {
 			}
 		}
 		
-		return count + 1; //return the idex of unique value print till there
+		return count + 1; //return the index of unique value print till there
 		
 	}
 
-    static void removedup(int[] arr){
-
-        int k=0;
-
-        for(int i=0; i<arr.length; i++){
-
-            boolean seen = false;
-            for(int j=0; j<k; j++){
-                if(arr[i] == arr[j]){
-                   seen = true;
-                   break;
-                }
-            }
-
-
-            if(!seen){
-                arr[k] = arr[i];
-                k++;
-            }
-        }
-
+    static void removedup(int[] arr) { //unsorted and manual
+		
+		int k =0; 
+		
+		for(int i=0; i<arr.length; i++) { // i → current element (full array loop) outer loop
+			
+			boolean seen = false;
+			
+			for(int j=0; j<k ; j++) { //j → checking only previous unique elements inner loop
+				if(arr[i] == arr[j]) {
+					seen =true;
+					break;
+				}
+				
+			}
+			
+			if(!seen) {
+				arr[k] = arr[i]; // if not seen then add to front side of array
+				k++;
+			}
+			
+		}
+		
 		for(int i=0; i<k; i++) {
 			System.out.println(arr[i]);
 		}
+	}
 
-    }
+
+    static void usingset(int[] arr) { // using set and take extra space
+		
+		Set<Integer> s=new  HashSet<Integer>();
+		
+		for(int num : arr) {
+			s.add(num);
+		}
+		
+		System.out.println(s);
+		
+	}
+
+//		Depends on constraints:
+//
+//		Sorted → two pointer
+//		Unsorted + space → HashSet
+//		Unsorted + no space → O(n²) manual
 
 }
