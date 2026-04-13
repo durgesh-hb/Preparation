cb37cd7e713a3a94b26c74489e4be2b6a91392ec
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-04-13 12:40:34 +0530

    if not sorted but takes extra space

--- java/questions/arrays/remove_duplicate.java
+++ java/questions/arrays/remove_duplicate.java
@@ -4,12 +4,10 @@ public class remove_duplicate {
     public static void main(String[] args) {
         
         int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
-        int ans = remove(arr);
+        removedup(arr);
 
-        for(int i=0; i<ans; i++){
-            System.out.println(arr[i]);
-        }
-
+        
+        
     }
 
     static int remove(int[] arr) {
@@ -27,5 +25,32 @@ public class remove_duplicate {
 		return count + 1; //return the idex of unique value print till there
 		
 	}
+
+    static void removedup(int[] arr){
+
+        int k=0;
+
+        for(int i=0; i<arr.length; i++){
+
+            boolean seen = false;
+            for(int j=0; j<k; j++){
+                if(arr[i] == arr[j]){
+                   seen = true;
+                   break;
+                }
+            }
+
+
+            if(!seen){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+
+		for(int i=0; i<k; i++) {
+			System.out.println(arr[i]);
+		}
+
+    }
 
 }
