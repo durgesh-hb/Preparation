e4a365fed33a16c90fbaf9d49ab2659f1af95de1
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-04-14 18:59:05 +0530

    simple reading & writing example

---
+++ java/file_handling/IO_stream.java
@@ -0,0 +1,13 @@
+package file_handling;
+import java.util.Scanner;
+public class IO_stream {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in); //systen.in is input stream
+		
+		System.out.println("Enter the number = ");
+		int n=sc.nextInt();
+		System.out.println(n); // System.out is output stream
+	}
+
+}
