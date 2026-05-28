c18a66461bfa35e76a6f480d9842ca67b77c7e6a
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-05-28 19:01:36 +0530

    for 3rd occurrence code also added

--- java/searching/binary/find_first_last_occurrence.java
+++ java/searching/binary/find_first_last_occurrence.java
@@ -33,9 +33,9 @@ public class find_first_last_occurrence {
 				ans = mid;
 				
 				if(first) {
-					end = mid - 1;
+					end = mid - 1; // move full left
 				}else {
-					start = mid + 1;
+					start = mid + 1; // move full right
 				}
 				
 			}
@@ -43,5 +43,69 @@ public class find_first_last_occurrence {
 		}
 		return ans;
 	}
+
+//    for first, last and 3rd occurrence
+	
+//	  int[] arr = {1,2,2,2,2,3,4};
+//      int target = 2;
+//      int k = 3;
+//
+//      int first = search(arr, target, true);
+//
+//      // target not found
+//      if(first == -1) {
+//	    System.out.println("Target not found");
+//	    return;
+//	}
+//	
+//		int count = last - first + 1;
+//	
+//		check kth occurrence exists or not
+//		if(k > count) {
+//	    	System.out.println(k + "th occurrence does not exist");
+//		}
+//		else {
+//	
+//	    int index = first + (k - 1);
+//	
+//	    System.out.println(k + "th occurrence index is: " + index);
+//}
+//
+//  }
+//
+//  static int search(int[] arr, int target, boolean findFirst) {
+//
+//      int start = 0;
+//      int end = arr.length - 1;
+//      int ans = -1;
+//
+//      while(start <= end) {
+//
+//          int mid = start + (end - start) / 2;
+//
+//          if(arr[mid] < target) {
+//              start = mid + 1;
+//          }
+//
+//          else if(arr[mid] > target) {
+//              end = mid - 1;
+//          }
+//
+//          else {
+//
+//              ans = mid;
+//
+//              if(findFirst) {
+//                  end = mid - 1; // move LEFT
+//              }
+//
+//              else {
+//                  start = mid + 1; // move RIGHT
+//              }
+//          }
+//      }
+//
+//      return ans;
+  
 	
 }
