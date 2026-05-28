960a1e12027dcff866c22b9e6eacdbabcaf165c7
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-05-28 17:52:16 +0530

    added the sorted array for binary search and comment's

--- java/searching/binary_search.java
+++ java/searching/binary_search.java
@@ -1,13 +1,13 @@
 package searching;
 public class binary_search{
     public static void main(String[] args) {
-        int[] arr={4, 6, 1, 2, 16, 7, 8, 12,};
+        int[] arr={1,2,4,6,7,8,12,16};
         int target=12;
         int ans=biinear_search(arr, target);
         System.out.println(ans);
     }
     //Divide the array → check → move left/right → repeat 
-    
+    //array should always to be sorted for binary search
     static int biinear_search(int[] arr, int target){
          int start = 0;
          int end = arr.length - 1;
