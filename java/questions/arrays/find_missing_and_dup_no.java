43ba8df9472ead848c6c844896f0c19a5fa737df
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-05-29 17:56:20 +0530

    updated

---
+++ java/questions/arrays/find_missing_and_dup_no.java
@@ -0,0 +1,47 @@
+package questions.arrays;
+
+public class find_missing_and_dup_no {
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 3, 0, 1};
+		int ans = missing(arr);
+		System.out.println(ans);
+		
+	}
+	//only 0 to n
+	static int missing(int[] arr) {
+		
+		int xor = arr.length;
+		for(int i=0; i<arr.length; i++) {
+			
+			xor = xor ^ i;
+			xor = xor ^ arr[i];
+		}
+		return xor;
+		
+	}
+//use this for most times
+
+// can also do from binary search
+	// int lo = 0, hi = nums.length;
+    // while (lo < hi) {
+    //     int mid = lo + (hi - lo) / 2;
+    //     if (nums[mid] > mid) hi = mid;
+    //     else lo = mid + 1;
+    // }
+    // return lo;
+    
+// Missing Number (0 to n)
+// Best Approaches:
+// 1. XOR -> O(n), O(1)
+// 2. Cyclic Sort -> O(n), O(1)
+// 3. binary search (if sorted)
+
+// Duplicate Number
+// 1. HashMap / HashSet -> O(n), O(n)
+// 2. Nested loops -> O(n²), O(1)
+// 3. Cyclic Sort -> O(n), O(1) (most common interview approach)
+
+}
