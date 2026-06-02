6f5ffdf5ba1eee0821bdf8447bd70bb460549783
DurgeshGowda-HB <durgeshgowda231@gmail.com>
2026-06-02 11:15:19 +0530

    updated

--- java/questions/arrays/sub_array/array_rotation.java
+++ java/questions/arrays/sub_array/array_rotation.java
@@ -3,38 +3,45 @@ package questions.arrays.sub_array;
 import java.util.Arrays;
 
 public class array_rotation {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int[] arr = {1, 2, 3, 4, 5, 6};
-		rotate(arr, 3);
+		int k=2;
+		rotation(arr, k);
 		System.out.println(Arrays.toString(arr));
+		
 	}
-	
-	static void rotate(int[] arr, int k) {
+	static  void rotation(int[] arr, int k) {
 		
-		int n=arr.length;
-		int k1=k%n;
+		int n= arr.length;
+		int k1=k%n; // only use this if have larger k ex = 8
 		
-		reverse(arr, 0, n-1);
-		reverse(arr, 0, k-1);
-		reverse(arr, k, n-1);
 		
-	//  left rorate
+		//right rotation
+//		reverse(arr, 0, n-1);
 //		reverse(arr, 0, k-1);
 //		reverse(arr, k, n-1);
-//		reverse(arr, 0, n-1);
 		
-//		👉 Right rotate:
-//
-//			FULL → LEFT PART → RIGHT PART
-//
-//		👉 Left rotate:
-//
-//			LEFT PART → RIGHT PART → FULL
+		//left rotation
+		reverse(arr, 0, k-1);
+		reverse(arr, k, n-1);
+		reverse(arr, 0, n-1);
 		
+//		Right Rotation =
+//
+//		Reverse whole array
+//		Reverse first k elements
+//		Reverse remaining elements
+//
+//		Left Rotation =
+//
+//		Reverse first k elements
+//		Reverse remaining elements
+//		Reverse whole array
+	
 	}
-
+	
 	static void reverse(int[] arr, int l, int r) {
 		
 		while(l < r) {
