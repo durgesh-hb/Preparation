bd7a4215e10ceb2da0bb4f2569f0335704d01832
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-07-08 16:06:52 +0530

    fiind the maximum node in tree

---
+++ java/questions/recursion/tree/maximum_value_in_tree.java
@@ -0,0 +1,43 @@
+package questions.recursion.tree;
+
+public class maximum_value_in_tree {
+    static class node{
+		int data;
+		node left;
+		node right;
+	
+	node(int data){
+		this.data= data;
+		this.left= null;
+		this.right= null;
+	}
+	}
+
+	static int maximum_value(node root) {
+			
+		if(root == null) {
+			return Integer.MIN_VALUE;
+		}
+		
+		int left = maximum_value(root.left);
+		int right = maximum_value(root.right);
+		
+		return Math.max(left, Math.max(root.data, right));
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		node root = new node(50);
+		
+		root.left = new node(30);
+		root.right = new node(80);
+		
+		root.left.left = new node(20);
+		
+		System.out.println(maximum_value(root));
+		
+	}
+}
