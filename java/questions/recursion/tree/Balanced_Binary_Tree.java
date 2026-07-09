54e20d30064792f5062871e1c788e47d4dc2a21e
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-07-09 15:24:47 +0530

    balanced tree or not

---
+++ java/questions/recursion/tree/Balanced_Binary_Tree.java
@@ -0,0 +1,63 @@
+package questions.recursion.tree;
+
+public class Balanced_Binary_Tree {
+    static class node{
+		int data;
+		node left;
+		node right;
+	
+	node(int data){
+		this.data= data;
+		this.left= null;
+		this.right= null;
+	}
+	}
+	
+	static int height(node root) {
+		
+		if(root == null) {
+			return 0;
+		}
+		
+		int left = height(root.left);
+		int right = height(root.right);
+		
+		return Math.max(left, right) + 1;
+		
+	}
+	
+	static boolean isbalanced(node root) {
+		
+		if(root == null) {
+			return true;
+		}
+		
+		int left = height(root.left);
+		int right = height(root.right);
+		
+		if(Math.abs(right - left) > 1) {
+			return false;
+		}
+		
+		return isbalanced(root.left) && isbalanced(root.right);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		node root = new node(50);
+		
+		root.left = new node(30);
+//		root.right = new node(80);
+		
+		root.left.left = new node(20);
+//		root.left.right = new node(40);
+		
+//		root.right.left = new node(70);
+//		root.right.right = new node(90);
+		
+		System.out.println(isbalanced(root));
+		
+	}
+}
