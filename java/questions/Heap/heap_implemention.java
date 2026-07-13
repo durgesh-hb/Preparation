43023957226ac1266b500ee5c52875c00fe219c2
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-07-13 15:49:52 +0530

    heap deletion heapify down

--- java/questions/Heap/heap_implemention.java
+++ java/questions/Heap/heap_implemention.java
@@ -26,11 +26,50 @@ public class heap_implemention {
 				}
 			}		
 		}
+
+		int delete(){
+
+			int deleted = heap.get(0);
+			
+			int last = heap.get(heap.size()-1);
+			
+			heap.set(0, last);
+			
+			heap.remove(heap.size()-1);
+			
+			int current = 0;
+			
+			while(2 * current + 1 < heap.size()) {
+				
+				int left = 2 * current + 1;
+				int right = 2 * current + 2;
+				
+				// assume first left is smaller 
+				int smallest = left;
+				
+				// if right is smaller let it prove it self? 
+				if(right < heap.size() && heap.get(right) < heap.get(left)) {
+					smallest = right;
+				}
+				
+				// now we now the which child is smaller
+				
+				//now normal get the real value compare and swap
+				if(heap.get(current) > heap.get(smallest)){
+					Collections.swap(heap, current, smallest);
+					current = smallest;
+				}else{
+					break;
+				}
+
+			}
+			return deleted;
+		}
 		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
