aa4200f954f85a6553db6c942eab52c18423ea29
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-07-16 13:24:09 +0530

    added the auto way for adding the graph

--- java/questions/graph/graph_implementation.java
+++ java/questions/graph/graph_implementation.java
@@ -6,7 +6,8 @@ public class graph_implementation {
 
     public static void main(String[] args) {
 		
-		graph();
+	//	graph();
+        graph_auto();
 	}
 	
 	public static void graph() {
@@ -32,6 +33,36 @@ public class graph_implementation {
 	            System.out.println(i + " -> " + graph.get(i));
 	        }
 		
+	}
+
+    public static void graph_auto() {
+		
+		int vertices = 5;
+		
+		int[][] edge = {
+				{0, 1},
+				{0, 2},
+				{1, 3},
+				{2, 4}
+		};
+		
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		
+		for(int i=0; i<vertices; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for(int[] edges : edge) {
+			int u = edges[0];
+			int v = edges[1];
+			
+			graph.get(u).add(v);
+			graph.get(v).add(u); //undirection
+		}
+		
+		for(int i = 0; i < vertices; i++) {
+            System.out.println(i + " -> " + graph.get(i));
+        }
 	}
     
 }
