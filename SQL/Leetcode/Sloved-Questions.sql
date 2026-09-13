409b3c38f8d79892fe3ffbf18bf537abb3c09d38
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-09-13 23:27:04 +0530

    updated

--- SQL/Leetcode/Sloved-Questions.sql
+++ SQL/Leetcode/Sloved-Questions.sql
@@ -1,7 +1,8 @@
-# Write your MySQL query statement below
+197. Rising Temperature
 
 select w1.id
 from Weather w1
 join weather w2
  ON w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
 where w1.temperature > w2.temperature;
+
