27ffe441e23324ee2acd572fcf944925d4d3bd9e
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-09-13 23:29:04 +0530

    customers who never oerded

--- SQL/Leetcode/Sloved-Questions.sql
+++ SQL/Leetcode/Sloved-Questions.sql
@@ -6,3 +6,10 @@ join weather w2
  ON w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
 where w1.temperature > w2.temperature;
 
+183. Customers Who Never Order
+
+select c.name as Customers
+from customers c
+left join Orders o
+on c.id = o.CustomerID
+where o.id is null;
