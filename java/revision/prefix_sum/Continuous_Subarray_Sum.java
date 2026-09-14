ae4c11026368117d76aa058d8b1182321cd9b9e9
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-09-14 12:51:17 +0530

    continuius subarray sum

--- java/revision/prefix_sum/Continuous_Subarray_Sum.java
+++ java/revision/prefix_sum/Continuous_Subarray_Sum.java
@@ -1,7 +1,42 @@
 package revision.prefix_sum;
 
+import java.util.HashMap;
+
 public class Continuous_Subarray_Sum {
 
+    public static void main(String[] args) {
 
+        int[] arr = {23,2,4,6,7};
+        int k = 6;
+        System.out.println(checkSubarraySum(arr,k));
+        
+    }
     
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        map.put(0, -1);
+        for(int i=0; i<nums.length; i++){
+
+            sum += nums[i];
+            int reminder = sum % k;
+
+            if(map.containsKey(reminder)){
+                int currentindex = i;
+                int previousindex = map.get(reminder);
+                int length = currentindex - previousindex;
+
+                if(length >= 2){
+                    return true;
+                }   
+            }else{
+                map.put(reminder, i);
+            }
+        }
+
+        return false;
+    }
+
 }
