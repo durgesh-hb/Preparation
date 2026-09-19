f20a3c7686d240dd3d4d7d523fc8d3ac09dac4b1
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-09-19 08:00:00 +0530

    577 join

--- SQL/Leetcode/Sloved-Questions.sql
+++ SQL/Leetcode/Sloved-Questions.sql
@@ -70,3 +70,10 @@ select player_id , min(event_date) as first_login
 from activity
 group by player_id;
 
+577 
+select e.name, b.bonus
+from employee e
+left join bonus b
+on e.empid = b.empid
+where b.bonus <= 1000 or b.bonus is null;
+
